056ae750e761b5147738f255d0298a34142a9dc3
Guru Murthy C L <clguru.murthy007@gmail.com>
2022-05-01 12:55:44 +0530

    Updated the main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 def print_hi():
     print("Hello PyCharm!!!")
+    print("I'm practicing Apache beam Examples!!!")
     print(random.choice(range(900,1000)))
 
 if __name__=='__main__':
